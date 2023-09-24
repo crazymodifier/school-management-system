0da2e0955b6a141e8ac2e2a23bda3acb000bf045
Suraj Kumar <surajkumar@Surajs-MacBook-Pro.local>
2023-09-24 13:25:09 +0530

    Day-29

--- student/fee-details.php
+++ student/fee-details.php
@@ -5,6 +5,7 @@
 
 <?php
 $success_msg =  false;
+$user_id = isset($_SESSION['user_id'])?$_SESSION['user_id']:"";
 if(isset($_POST['form_submitted'])){
 
     $status = isset($_POST["status"])?$_POST["status"]:'success';
@@ -13,7 +14,7 @@ if(isset($_POST['form_submitted'])){
     $email = isset($_POST["email"])?$_POST["email"]:'';
     $month = isset($_POST["month"])?$_POST["month"]:'';
 
-    $user_id = isset($_SESSION['user_id'])?$_SESSION['user_id']:"";
+    
 
     $title = $month.' - Fee';
     
@@ -36,6 +37,149 @@ if(isset($_POST['form_submitted'])){
     
     $success_msg = true;
 }
+
+if($_GET['action'] == 'view'){ ?>
+
+
+<div class="container">
+<div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="invoice-title">
+                        <h4 class="float-end font-size-15">Invoice #DS0204 <span class="badge bg-success font-size-12 ms-2">Paid</span></h4>
+                        <div class="mb-4">
+                           <h2 class="mb-1 text-muted">Bootdey.com</h2>
+                        </div>
+                        <div class="text-muted">
+                            <p class="mb-1">3184 Spruce Drive Pittsburgh, PA 15201</p>
+                            <p class="mb-1"><i class="uil uil-envelope-alt me-1"></i> xyz@987.com</p>
+                            <p><i class="uil uil-phone me-1"></i> 012-345-6789</p>
+                        </div>
+                    </div>
+
+                    <hr class="my-4">
+
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="text-muted">
+                                <h5 class="font-size-16 mb-3">Billed To:</h5>
+                                <h5 class="font-size-15 mb-2">Preston Miller</h5>
+                                <p class="mb-1">4068 Post Avenue Newfolden, MN 56738</p>
+                                <p class="mb-1">PrestonMiller@armyspy.com</p>
+                                <p>001-234-5678</p>
+                            </div>
+                        </div>
+                        <!-- end col -->
+                        <div class="col-sm-6">
+                            <div class="text-muted text-sm-end">
+                                <div>
+                                    <h5 class="font-size-15 mb-1">Invoice No:</h5>
+                                    <p>#DZ0112</p>
+                                </div>
+                                <div class="mt-4">
+                                    <h5 class="font-size-15 mb-1">Invoice Date:</h5>
+                                    <p>12 Oct, 2020</p>
+                                </div>
+                                <div class="mt-4">
+                                    <h5 class="font-size-15 mb-1">Order No:</h5>
+                                    <p>#1123456</p>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- end col -->
+                    </div>
+                    <!-- end row -->
+                    
+                    <div class="py-2">
+                        <h5 class="font-size-15">Order Summary</h5>
+
+                        <div class="table-responsive">
+                            <table class="table align-middle table-nowrap table-centered mb-0">
+                                <thead>
+                                    <tr>
+                                        <th style="width: 70px;">No.</th>
+                                        <th>Item</th>
+                                        <th>Price</th>
+                                        <th>Quantity</th>
+                                        <th class="text-end" style="width: 120px;">Total</th>
+                                    </tr>
+                                </thead><!-- end thead -->
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">01</th>
+                                        <td>
+                                            <div>
+                                                <h5 class="text-truncate font-size-14 mb-1">Black Strap A012</h5>
+                                                <p class="text-muted mb-0">Watch, Black</p>
+                                            </div>
+                                        </td>
+                                        <td>$ 245.50</td>
+                                        <td>1</td>
+                                        <td class="text-end">$ 245.50</td>
+                                    </tr>
+                                    <!-- end tr -->
+                                    <tr>
+                                        <th scope="row">02</th>
+                                        <td>
+                                            <div>
+                                                <h5 class="text-truncate font-size-14 mb-1">Stainless Steel S010</h5>
+                                                <p class="text-muted mb-0">Watch, Gold</p>
+                                            </div>
+                                        </td>
+                                        <td>$ 245.50</td>
+                                        <td>2</td>
+                                        <td class="text-end">$491.00</td>
+                                    </tr>
+                                    <!-- end tr -->
+                                    <tr>
+                                        <th scope="row" colspan="4" class="text-end">Sub Total</th>
+                                        <td class="text-end">$732.50</td>
+                                    </tr>
+                                    <!-- end tr -->
+                                    <tr>
+                                        <th scope="row" colspan="4" class="border-0 text-end">
+                                            Discount :</th>
+                                        <td class="border-0 text-end">- $25.50</td>
+                                    </tr>
+                                    <!-- end tr -->
+                                    <tr>
+                                        <th scope="row" colspan="4" class="border-0 text-end">
+                                            Shipping Charge :</th>
+                                        <td class="border-0 text-end">$20.00</td>
+                                    </tr>
+                                    <!-- end tr -->
+                                    <tr>
+                                        <th scope="row" colspan="4" class="border-0 text-end">
+                                            Tax</th>
+                                        <td class="border-0 text-end">$12.00</td>
+                                    </tr>
+                                    <!-- end tr -->
+                                    <tr>
+                                        <th scope="row" colspan="4" class="border-0 text-end">Total</th>
+                                        <td class="border-0 text-end"><h4 class="m-0 fw-semibold">$739.00</h4></td>
+                                    </tr>
+                                    <!-- end tr -->
+                                </tbody><!-- end tbody -->
+                            </table><!-- end table -->
+                        </div><!-- end table responsive -->
+                        <div class="d-print-none mt-4">
+                            <div class="float-end">
+                                <a href="javascript:window.print()" class="btn btn-success me-1"><i class="fa fa-print"></i></a>
+                                <a href="#" class="btn btn-primary w-md">Send</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><!-- end col -->
+    </div>
+</div>
+<?php
+
+
+}
+else{
 
 ?>
 
@@ -99,25 +243,53 @@ if(isset($_POST['form_submitted'])){
               </thead>
               <tbody>
                 <?php
+
+                $sql = "SELECT m.meta_value as `month` FROM `posts` as p JOIN `metadata` as m ON p.id = m.item_id WHERE p.type = 'payment' AND p.author = $user_id AND m.meta_key = 'month' AND year(p.publish_date) = 2023";
+
+                $query = mysqli_query($db_conn,$sql);
+                $paid_fees = [];
+                while($row = mysqli_fetch_object($query)){
+
+                  $paid_fees [] = strtolower($row->month);
+                  
+
+                }
+//  echo '<pre>';
+                   
+//                    print_r($paid_fees);
+//                    echo '</pre>'; 
                 $months = array('january', 'fabruary','march','april','may','june','july','august','september','october','november','december');
                 foreach ($months as $key => $value) {
                   $highlight = ''; 
-                  if(date('F') == ucwords($value))
-                  {
+
+                  $paid = false;
+                  if(in_array($value, $paid_fees)){
+                    $paid = true;
                     $highlight = 'class="bg-success"';
                   }
+                //   if(date('F') == ucwords($value))
+                //   {
+                //     $highlight = 'class="bg-success"';
+                //   }
                   ?>
                   <tr>
                     <td><?php echo ++$key?></td>
-                    <td <?php echo $highlight?>><?php echo ucwords($value)?></td>
-                    <td></td>
+                    <td ><?php echo ucwords($value)?></td>
+                    <td <?php echo $highlight?>>
+                        <?php
+
+                        echo ($paid) ? "Paid" : "Pending";
+
+                        ?>
+                    </td>
                     <td>
-                      <a href="?action=pay&month=<?php echo $value?>&std_id=<?php echo $std_id?>" class="btn btn-sm btn-primary"><i class="fa fa-eye fa-fw"></i> View</a>
-                      <a href="#" data-toggle="modal" data-month="<?php echo ucwords($value)?>" data-target="#paynow-popup" class="btn btn-sm btn-warning paynow-btn"><i class="fa fa-money-check-alt fa-fw"></i> Pay Now</a>
-
-                      <a href="?action=pay&month=<?php echo $value?>&std_id=<?php echo $std_id?>" class="btn btn-sm btn-dark"><i class="fa fa-envelope fa-fw"></i> Send Message</a>
-
-                      <a href="?action=pay&month=<?php echo $value?>&std_id=<?php echo $std_id?>" class="btn btn-sm btn-danger"><i class="fa fa-trash fa-fw"></i>Delete</a>
+                        <?php if($paid){ ?>
+                            <a href="?action=view&month=<?php echo $value?>&std_id=<?php echo $std_id?>" class="btn btn-sm btn-primary"><i class="fa fa-eye fa-fw"></i> View</a>
+                        <?php } else {?>
+                            <a href="#" data-toggle="modal" data-month="<?php echo ucwords($value)?>" data-target="#paynow-popup" class="btn btn-sm btn-warning paynow-btn"><i class="fa fa-money-check-alt fa-fw"></i> Pay Now</a>
+                        <?php } ?>
+                      
+                      
                     </td>
                   </tr>
                 <?php } ?>
@@ -194,4 +366,6 @@ if(isset($_POST['form_submitted'])){
         })
     </script>
     
-<?php include('footer.php') ?>
+<?php 
+}
+include('footer.php') ?>
