b858689b6cd31cec896af112a81dc80ce34ba559
Suraj <modifiercrazy@gmail.com>
2025-01-25 12:03:40 +0530

    user updated

--- admin/header.php
+++ admin/header.php
@@ -33,6 +33,10 @@
   <link rel="stylesheet" href="../plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
   <link rel="stylesheet" href="../plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
 
+  <link rel="stylesheet" href="../plugins/select2/css/select2.min.css">
+  <link rel="stylesheet" href="../plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+
+  <link rel="stylesheet" href="../plugins/calendar/zabuto_calendar.min.css">
 
   <!-- Theme style -->
   <link rel="stylesheet" href="../dist/css/adminlte.min.css">
